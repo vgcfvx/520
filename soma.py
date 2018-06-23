0079a960e5cd8108954b7339fb7411cbd6a7b588
vgcfvx <vfilho@vrvf.com.br>
2018-06-23 16:25:21 -0300

    aula1

--- soma.py
+++ soma.py
@@ -2,4 +2,10 @@
 nome = input("Nome do aluno: ")
 soma = int(input("Digite nota 1: "))
 soma += int(input("Digite nota 2: "))
-print("O Aluno {} tem a media {}".format(nome.title(),soma/2))
+print("{} tem a media {}".format(nome.title(),soma/2))
+if soma >= 14 :
+     print("{} foi aprovado".format(nome.title()))
+elif soma>8 and soma <14:
+     print("{} foi retido".format(nome.title()))
+else:
+     print("{} foi reprovado".format(nome.title()))     
